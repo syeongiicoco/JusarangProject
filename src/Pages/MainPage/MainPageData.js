19c4a34caa41eb7f4d285bbc45ee4d1862f67c16
syeongiicoco <syeongii78@gmail.com>
2021-05-27 19:08:11 +0900

    Add: mainpage 완료

---
+++ src/Pages/MainPage/MainPageData.js
@@ -0,0 +1,20 @@
+const WINESLIDE = [
+  {
+    id: 1,
+    img: './Images/MainPageImage/background1.jpg',
+  },
+  {
+    id: 2,
+    img: './Images/MainPageImage/background2.jpg',
+  },
+  {
+    id: 3,
+    img: './Images/MainPageImage/background3.jpg',
+  },
+  {
+    id: 4,
+    img: './Images/MainPageImage/background4.jpg',
+  },
+];
+
+export default WINESLIDE;
